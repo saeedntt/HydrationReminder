1cfdc17b1cd993dcdf7a86a6055dd8064d6a1a05
Nima Razavi <n.razavi70@gmail.com>
2020-12-16 16:04:36 +0330

    Entry Activiy Layout designed as requested with extra steps.

--- app/src/main/java/com/reminder/hydration/Result.kt
+++ app/src/main/java/com/reminder/hydration/Result.kt
@@ -6,14 +6,27 @@ sealed class Result {
 }
 
 operator fun Result.plus(other: Result): Result {
-    if (this is Result.Error)
+    return if (this > other)
+        this
+    else
+        other
+/*    if (this > other)
         return this
-    if (other is Result.Error)
-        return other
 
-    return this
+    return other
+
+ */
 }
 
-operator fun Result.compareTo(other :Result) : Int {
-
+operator fun Result.compareTo(other: Result): Int {
+    if ((this is Result.Error) and (other is Result.Ok))
+        return 1
+    if ((other is Result.Error) and (this is Result.Ok))
+        return -1
+    if ((this is Result.Ok) and (other is Result.Ok))
+        if (((this as Result.Ok).warning != "") and ((other as Result.Ok).warning == ""))
+            return 1
+    if (((this as Result.Ok).warning == "") and ((other as Result.Ok).warning != ""))
+        return -1
+    return 0
 }
