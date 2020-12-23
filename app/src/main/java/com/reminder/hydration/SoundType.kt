dd1002ad79d490d4ab3ba22c0ef59cc6b7516dd6
Nima Razavi <n.razavi70@gmail.com>
2020-12-23 17:24:26 +0330

    Fix Notif and Sound

--- app/src/main/java/com/reminder/hydration/SoundType.kt
+++ app/src/main/java/com/reminder/hydration/SoundType.kt
@@ -4,11 +4,6 @@ enum class SoundType(val value: Int) {
     SOUND(0),
     MUTE(1),
     VIBRATE(2);
-    /*SOUND(R.id.notificationSoundYes),
-    MUTE(R.id.notificationSoundNo),
-    VIBRATE(R.id.notificationSoundVibrate);
-
-     */
 
     companion object {
         fun of(value: Int): SoundType {
